f46ab2f8c12eeb4baae68a7d38cd67bd7413c0a5
musthafa <musthafa.cp@kalvium.community>
2024-02-15 15:09:58 +0530

    Every routes added

--- Routes/posts.js
+++ Routes/posts.js
@@ -5,15 +5,48 @@ const app = express();
 
 app.use(express.json());
 
+
+
+// GET
+
 router.get("/posts", async (req, res) => {
   try {
     const data = await postmodel.find();
     res.json(data);
   } catch (error) {
     console.error("An error occurred while getting the post details:", error);
-    res.status(500).json({ error: "Internal Server Error with the GET method of getting the post details" });
-  } 
+    res
+      .status(500)
+      .json({
+        error:
+          "Internal Server Error with the GET method of getting the post details",
+      });
+  }
 });
+
+
+
+// GET ACCORDING THE ID
+
+router.get("/posts/:id", async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const post = await postmodel.findById(postId);
+    res.json(post);
+  } catch (error) {
+    console.error("An error occurred while getting the post details:", error);
+    res
+      .status(500)
+      .json({
+        error:
+          "Internal Server Error with the GET method of getting the post details",
+      });
+  }
+});
+
+
+
+// POST
 
 router.post("/posts", async (req, res) => {
   try {
@@ -22,11 +55,111 @@ router.post("/posts", async (req, res) => {
     res.json(data);
   } catch (err) {
     console.log("An error is caught while posting the post details", err);
-    res.status(500).json({ error: "Internal Server Error with the POST method of submitting the post details " });
+    res
+      .status(500)
+      .json({
+        error:
+          "Internal Server Error with the POST method of submitting the post details ",
+      });
   }
 });
 
 
 
+// PUT
+
+router.put("/posts/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedData = await postmodel.findByIdAndUpdate(
+      id,
+      {
+        $set: {
+          title: req.body.title,
+          content: req.body.content,
+          link: req.body.link,
+        },
+      },
+      { new: true }
+    );
+
+    if (!updatedData) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    res.json(updatedData);
+  } catch (error) {
+    console.error("An error occurred while updating the post:", error);
+    res
+      .status(500)
+      .json({
+        error: "Internal Server Error with the PUT method while updating the post",
+      });
+  }
+});
+
+
+
+// PATCH
+
+router.patch("/posts/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const updateFields = {};
+    if (req.body.title) {
+      updateFields.title = req.body.title;
+    }
+    if (req.body.content) {
+      updateFields.content = req.body.content;
+    }
+    if (req.body.link) {
+      updateFields.link = req.body.link;
+    }
+
+    const updatedData = await postmodel.findByIdAndUpdate(
+      id,
+      {
+        $set: updateFields,
+      },
+      { new: true }
+    );
+
+    if (!updatedData) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    res.json(updatedData);
+  } catch (error) {
+    console.error("An error occurred while updating the post using PATCH:", error);
+    res
+      .status(500)
+      .json({
+        error: "Internal Server Error with the PATCH method of updating the post",
+      });
+  }
+});
+
+
+
+//DELETE
+
+router.delete("/posts/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    await postmodel.findByIdAndDelete(id);
+    res.status(201).json({
+      Message: "Deleted Succussfully",
+    });
+  } catch (err) {
+    console.log("An error occured in the deleting process of post details");
+    res
+      .status(500)
+      .json({
+        error:
+          "Intenal Server error with the deleting process off the post details",
+      });
+  }
+});
 
 module.exports = router;
--- Routes/user.js
+++ Routes/user.js
@@ -6,16 +6,50 @@ const app = express();
 app.use(express.json());
 
 
+
 // GET REQUEST
 router.get("/users", async (req, res) => {
   try {
     const data = await usermodel.find();
-    res.json(data); 
+    res.json(data);
   } catch (error) {
-    console.error("An error occurred with the GET method while getting the user data:", error);
-    res.status(500).json({ error: "Internal Server Error with the GET method while getting the user data" });
+    console.error(
+      "An error occurred with the GET method while getting the user data:",
+      error
+    );
+    res
+      .status(500)
+      .json({
+        error:
+          "Internal Server Error with the GET method while getting the user data",
+      });
   }
 });
+
+
+
+// GET REQUEST ACCORDING ID
+router.get("/users/:id", async (req, res) => {
+  try {
+    const userID = req.params.id;
+    const userData = await usermodel.findById(userID);
+
+    if (!userData) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json(userData);
+  } catch (error) {
+    console.error("An error occurred while getting the user details:", error);
+    res
+      .status(500)
+      .json({
+        error:
+          "Internal Server Error with the GET method of getting the user details",
+      });
+  }
+});
+
 
 
 // POST REQUEST
@@ -25,12 +59,107 @@ router.post("/users", async (req, res) => {
     const data = await usermodel.create(req.body);
     res.json(data);
   } catch (err) {
-    console.log("An error is caught with the POST method while posting the user data", err);
-    res.status(500).json({ error: "Internal Server Error with the POST method while posting the user data" });
+    console.log(
+      "An error is caught with the POST method while posting the user data",
+      err
+    );
+    res
+      .status(500)
+      .json({
+        error:
+          "Internal Server Error with the POST method while posting the user data",
+      });
   }
 });
 
 
 
+//PUT
+router.put("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedData = await usermodel.findByIdAndUpdate(
+      id,
+      {
+        $set: {
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+          phone_number: req.body.phone_number,
+        },
+      },
+      { new: true }
+    );
+
+    if (!updatedData) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json(updatedData);
+  } catch (error) {
+    console.error("An error occurred while updating the user:", error);
+    res
+      .status(500)
+      .json({
+        error: "Internal Server Error with the PUT method of updating the user",
+      });
+  }
+});
+
+
+
+// PATCH
+
+router.patch("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const updateFields = {};
+    if (req.body.name) {
+      updateFields.name = req.body.name;
+    }
+    if (req.body.email) {
+      updateFields.email = req.body.email;
+    }
+    if (req.body.password) {
+      updateFields.password = req.body.password;
+    }
+    if (req.body.phone_number) {
+      updateFields.phone_number = req.body.phone_number;
+    }
+
+    const updatedData = await usermodel.findByIdAndUpdate(
+      id,
+      {
+        $set: updateFields,
+      },
+      { new: true }
+    );
+
+    if (!updatedData) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json(updatedData);
+  } catch (error) {
+    console.error("An error occurred while updating the user:", error);
+    res
+      .status(500)
+      .json({
+        error:
+          "Internal Server Error with the PATCH method of updating the user",
+      });
+  }
+});
+
+//DELETE ACCORDING ID
+
+router.delete("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  await usermodel.findByIdAndDelete(id);
+  res.status(201).json({
+    Message: "Deleted Succussfully",
+  });
+});
 
 module.exports = router;
