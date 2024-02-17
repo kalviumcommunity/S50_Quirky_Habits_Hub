1dd36cf628500ad123d0c3e7d0823cfc39ededfa
musthafa <musthafa.cp@kalvium.community>
2024-02-17 15:38:36 +0530

    error handling in connect added

--- BackEnd/config/connect.js
+++ BackEnd/config/connect.js
@@ -4,10 +4,15 @@ const URI = process.env.URI;
 
 const connectDB = async () => {
   try {
+    if (!URI) {
+      throw new Error("MongoDB URI is missing. Please check your .env file.");
+    }
+
     await mongoose.connect(URI);
-    console.log("DataBase connected successfully");
+    console.log("Database connected successfully");
   } catch (err) {
-    console.error(err);
+    console.error(err.message);
+    process.exit(1);
   }
 };
 
